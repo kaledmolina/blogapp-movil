14bbebf6b92a3222ab14c590b80fba502d268a84
kaled molina <kaled.molina@campusucc.edu.co>
2023-10-23 17:45:08 -0500

    mejoras y logica con el server de la api para el login

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 const baseURL = 'http://192.168.43.109:8000/api';
 const loginURL = '$baseURL/login';
 const registerURL = '$baseURL/register';
@@ -9,3 +11,69 @@ const commentsURL = '$baseURL/comments';
 const serverError = 'Error del servidor, intente más tarde';
 const unauthorized = 'Credenciales incorrectas';
 const somethingWentWrong = 'Algo salió mal, intente más tarde';
+
+//---input decoration
+InputDecoration kInputDecoration(String label) {
+  return InputDecoration(
+    labelText: label,
+    labelStyle: const TextStyle(
+        color: Colors.purple), // Cambia el color del texto a purpura
+    contentPadding: const EdgeInsets.all(10),
+    border: const OutlineInputBorder(
+      borderSide: BorderSide(
+          width: 1,
+          color: Colors.purple), // Cambia el color del borde a purpura
+    ),
+    enabledBorder: const OutlineInputBorder(
+      borderSide: BorderSide(
+          color: Colors.purple,
+          width:
+              1.0), // Cambia el color del borde cuando el campo no está enfocado
+    ),
+    focusedBorder: const OutlineInputBorder(
+      borderSide: BorderSide(
+          color: Colors.purple,
+          width:
+              1.0), // Cambia el color del borde cuando el campo está enfocado
+    ),
+  );
+}
+
+//---button style
+TextButton kTextButton(String label, Function onPressed) {
+  return TextButton(
+    child: Text(
+      label,
+      style: TextStyle(color: const Color.fromARGB(255, 214, 202, 216)),
+    ),
+    style: ButtonStyle(
+      backgroundColor: MaterialStateProperty.all<Color>(
+          const Color.fromARGB(255, 219, 207, 221)),
+      padding: MaterialStateProperty.all<EdgeInsets>(
+          const EdgeInsets.fromLTRB(50, 10, 50, 10)),
+      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+        RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
+          side: const BorderSide(color: Colors.purple),
+        ),
+      ),
+    ),
+    onPressed: () => onPressed(),
+  );
+}
+
+//---row
+
+Row kLoginRegisterHint(String text, String label, Function onTap) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Text(text),
+      GestureDetector(
+        child: Text(label, style: TextStyle(color: Colors.purple),
+        ), 
+        onTap: () => onTap(),
+      )
+    ],
+  );
+}
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,4 +1,14 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:redapp/constant.dart';
+import 'package:redapp/model/api_response.dart';
+import 'package:redapp/model/user.dart';
+import 'package:redapp/screens/register.dart';
+import 'package:redapp/services/user_service.dart';
+import 'package:shared_preferences/shared_preferences.dart'; 
+import 'package:redapp/screens/home.dart';
+
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -8,48 +18,92 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  TextEditingController TxtEmail = TextEditingController();
+  TextEditingController TxtPassword = TextEditingController();
+
+  void _loginUser() async {
+    ApiResponse response = await login(TxtEmail.text, TxtPassword.text);
+    if (response.error == null ){
+      _saveAndRedirectToHome(response.data as User);
+    }
+    else{
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('${response.error}'),
+      ));
+    }
+  }
+  
+  void _saveAndRedirectToHome(User user) async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('token', user.token ?? '');
+    await prefs.setInt('userId', user.id ?? 0);
+    Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder:(context)=>const Home()),(route)=>false);
+
+    }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.purple,
-        title: const Text('Iniciar sesión'),
+        title: const Text('Ingresa a tu cuenta'),
         centerTitle: true,
       ),
       body: Form(
+        key: formKey,
         child: ListView(
           padding: const EdgeInsets.all(35),
           children: [
             TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Correo electrónico',
-                labelStyle: TextStyle(
-                    color:
-                        Colors.purple), // Cambia el color del texto a amarillo
-                contentPadding: EdgeInsets.all(10),
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(
-                      width: 1,
-                      color: Colors
-                          .purple), // Cambia el color del borde a amarillo
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                      color: Colors.purple,
-                      width:
-                          1.0), // Cambia el color del borde cuando el campo no está enfocado
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                      color: Colors.purple,
-                      width:
-                          1.0), // Cambia el color del borde cuando el campo está enfocado
-                ),
-              ),
+              keyboardType: TextInputType.emailAddress,
+              controller: TxtEmail,
+              validator: (val) =>
+                  val!.isEmpty ? 'correo electrónico requerido' : null,
+              decoration: kInputDecoration("Correo electrónico"),
               style: const TextStyle(
                   color: Colors
-                      .purple), // Cambia el color del texto ingresado a amarillo
+                      .purple), // Cambia el color del texto ingresado a purpura
             ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              controller: TxtPassword,
+              obscureText: true,
+              validator: (val) =>
+                  val!.isEmpty ? 'Requiere al menos 6 caracteres' : null,
+              decoration: kInputDecoration("Contraseña"),
+              style: const TextStyle(
+                  color: Colors
+                      .purple), // Cambia el color del texto ingresado a purpura
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextButton(
+              child: Text(
+                'Ingresar',
+                style:
+                    TextStyle(color: const Color.fromARGB(255, 214, 202, 216)),
+              ),
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateColor.resolveWith((states) => Colors.purple),
+                padding: MaterialStateProperty.resolveWith(
+                    (states) => EdgeInsets.symmetric(vertical: 10)),
+              ),
+              onPressed: () {
+                if (formKey.currentState!.validate()) {
+                  _loginUser();
+                }
+              },
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            kLoginRegisterHint ('¿No tienes una cuenta? ', 'Regístrate', (){
+              Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder:(context)=> Register()),(route)=>false);
+            }),
           ],
         ),
       ),
