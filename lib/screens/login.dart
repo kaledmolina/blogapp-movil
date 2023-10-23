be89748fab942612f8bcc51275984027adcbf80b
kaled molina <kaled.molina@campusucc.edu.co>
2023-10-23 13:49:03 -0500

    vista login input correo

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -10,9 +10,48 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(' hola Login'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title: const Text('Iniciar sesión'),
+        centerTitle: true,
+      ),
+      body: Form(
+        child: ListView(
+          padding: const EdgeInsets.all(35),
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Correo electrónico',
+                labelStyle: TextStyle(
+                    color:
+                        Colors.purple), // Cambia el color del texto a amarillo
+                contentPadding: EdgeInsets.all(10),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                      width: 1,
+                      color: Colors
+                          .purple), // Cambia el color del borde a amarillo
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                      color: Colors.purple,
+                      width:
+                          1.0), // Cambia el color del borde cuando el campo no está enfocado
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                      color: Colors.purple,
+                      width:
+                          1.0), // Cambia el color del borde cuando el campo está enfocado
+                ),
+              ),
+              style: const TextStyle(
+                  color: Colors
+                      .purple), // Cambia el color del texto ingresado a amarillo
+            ),
+          ],
+        ),
       ),
     );
   }
